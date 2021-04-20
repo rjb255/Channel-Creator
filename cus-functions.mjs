4a6fd944e052f3dc69086489233f2cbf224e4109
rjb255 <rjb255@cam.ac.uk>
2021-04-20 01:40:17 +0100

    Finalised v1

--- cus-functions.mjs
+++ cus-functions.mjs
@@ -1,3 +1,5 @@
+import fs from "fs";
+
 function serverConfig(server, globals) {
     config = globals[config];
     if (config[server.id]) {
@@ -12,26 +14,76 @@ function serverConfig(server, globals) {
 
 function checkThrough(messages, server, globals) {
     messages.forEach((msg) => {
-        let channel = server.channels.cache.find((c) => c.parentID == server.categoryId.id && c.name == msg.content);
-        if (channel) {
-            channel.lockPermissions().then(function () {
-                let red = msg.reactions.cache.users
-                    .fetch()
-                    .then(() => reaction.cache.map((item) => item.users.cache.array()));
-                red.forEach((r) => channel.updateOverwrite(r.id, { VIEW_CHANNEL: true }));
-            });
+        if (globals["relations"][msg.id]) {
+            let channel = server.channels.cache.find((c) => c.id == msg.globals["relations"][msg.id]);
+            if (channel) {
+                channel.lockPermissions().then(function () {
+                    let red = msg.reactions.cache.users
+                        .fetch()
+                        .then(() => reaction.cache.map((item) => item.users.cache.array()));
+                    red.forEach((r) => channel.updateOverwrite(r.id, { VIEW_CHANNEL: true }));
+                });
+            } else {
+                console.error("Error in relations json");
+            }
         } else {
             let red = msg.reactions.cache.users;
             if (red.cache.size > 0) {
+                channelCreator(msg, server, globals);
             }
         }
     });
 }
 
+function channelCreator(msg, server, globals) {
+    let bot = globals["bot"];
+    let category = server.categoryId;
+    server.channels
+        .create(msg.content, {
+            type: "text",
+            permissionOverwrites: [
+                {
+                    id: server.id,
+                    deny: ["VIEW_CHANNEL"],
+                },
+                {
+                    id: msg.author.id,
+                    allow: ["VIEW_CHANNEL"],
+                },
+                {
+                    id: bot.user.id,
+                    allow: ["VIEW_CHANNEL"],
+                },
+            ],
+            parent: category,
+        })
+        .then((c) => {
+            let duplicate = c.guild.channels.cache.find(
+                (channel) => c.id != channel.id && channel.parentID == category.id && c.name == channel.name
+            );
+            if (duplicate) {
+                c.delete();
+                globals["relations"][msg.id] = duplicate.id;
+            } else {
+                globals["relations"][msg.id] = c.id;
+                c.send(
+                    `Welcome to this server. Please use this as a place to talk & prepare activities related to the title of this server.` +
+                        `Don't be shy. We are all friends here xx.`
+                );
+            }
+            updateRel(globals);
+        })
+        .catch(console.error);
+}
+
+function updateRel(globals) {
+    fs.writeFile("./serverConfig/message2channel.json", globals["relations"]);
+}
+
 function reactionChange(reaction, user, state, globals) {
-    if (serverConfig(server, config) === true) {
-        server.mainChannel.messages.fetch().then((m) => checkThrough(m, server));
+    if (serverConfig(server, config, globals) === true) {
+        server.mainChannel.messages.fetch().then((m) => checkThrough(m, server, globals));
     }
 }
 
-export { serverConfig };
+export { serverConfig, checkThrough, channelCreator, updateRel, reactionChange };
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"; // Securely get env variables
 dotenv.config();
 import fs from "fs"; // File reading even upon modification
 import Discord from "discord.js"; // Discord Specific Stuff
-import { serverConfig } from "cus-functions"; // Custom Functions
+import { serverConfig, checkThrough, reactionChange } from "cus-functions"; // Custom Functions
 
 //Defining characteristics async
 let config = {};
@@ -16,7 +16,7 @@ const [, , TOKEN, testcategoryId, testmainChannel, bot] = await Promise.resolve(
     "795982405260410880",
     new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] }),
 ]);
-global = { config, relations };
+global = { config, relations, bot };
 bot.login(TOKEN);
 
 //Update the server after a hiatus
@@ -35,63 +35,16 @@ bot.on("message", (msg) => {
     if (!serverProperties(server, globals)) break;
     if (msg.content == "restart" && server.mainChannel == testmainChannel) {
         clear(server);
-    } else {
-        let channel = msg.channel.id;
-        if (channel === server.mainChannel.id && msg.author.id != bot.user.id) {
-            let category = server.channels.cache.find((c) => c.id == server.categoryId.id && c.type == "category");
-            server.channels
-                .create(msg.content, {
-                    type: "text",
-                    permissionOverwrites: [
-                        {
-                            id: server.id,
-                            deny: ["VIEW_CHANNEL"],
-                        },
-                        {
-                            id: msg.author.id,
-                            allow: ["VIEW_CHANNEL"],
-                        },
-                        {
-                            id: bot.user.id,
-                            allow: ["VIEW_CHANNEL"],
-                        },
-                    ],
-                    parent: category,
-                })
-                .then((c) => channelCreated(c, msg, category))
-                .catch(console.error);
-
-            const filter = (reaction, user) => {
-                return reaction.emoji.name;
-            };
-
-            const collector = msg.createReactionCollector(filter);
-            collector.on("collect", function (collected) {
-                let users = collected.users.cache;
-                let destinedChannel = server.channels.cache.find(
-                    (c) => c.name == msg.content && c.type == "text" && c.parentID == server.categoryId.id
-                );
-                for (user of users) {
-                    console.log(collected);
-                    destinedChannel.updateOverwrite(user[0], { VIEW_CHANNEL: true });
-                }
-            });
-        }
     }
 });
 
-function channelCreated(channel, msg, category) {
-    let duplicate = channel.guild.channels.cache.find(
-        (c) => c.id != channel.id && c.parentID == category.id && c.name == channel.name
-    );
-    if (duplicate.size) {
-        channel.delete();
-    } else {
-        msg.channel
-            .send(`Welcome to this server. Please use this as a place to talk & prepare activities related to the title of this server.
-Don't be shy. We are all friends here xx.`);
-    }
-}
+bot.on("messageReactionAdd", async (reaction, user) => {
+    reactionChange(reaction, user, 1, globals);
+});
+
+bot.on("messageReactionRemove", async (reaction, user) => {
+    reactionChange(reaction, user, 0, globals);
+});
 
 async function clear(server) {
     for (c of server.channels.cache) {
@@ -108,11 +61,3 @@ async function clear(server) {
             .catch(console.error);
     }
 }
-
-bot.on("messageReactionAdd", async (reaction, user) => {
-    reactionChange(reaction, user, 1);
-});
-
-bot.on("messageReactionRemove", async (reaction, user) => {
-    reactionChange(reaction, user, 0);
-});
